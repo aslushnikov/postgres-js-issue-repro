a987c1f7860b2084db1745e0e98f57cbe713f98b
Andrey Lushnikov <aslushnikov@gmail.com>
2024-03-20 12:11:29 -0700

    devops: add github actions

--- test.mjs
+++ test.mjs
@@ -9,13 +9,24 @@ process.env.PGUSER = 'user';
 process.env.PGPASSWORD = 'password';
 process.env.PGDATABASE = 'postgres';
 
-const sql = postgres({ max: 1 });
-await sql.file('./bad.sql');
-await sql.end();
+{
+  const sql = postgres({ max: 1 });
+  await sql.file('./bad.sql').then(() => {
+    console.log('postgres.js: No error!');
+  }).catch(e => {
+    console.log('postgres.js: yes error!', e.message);
+  });
+  await sql.end();
+}
 
-/*
-const client = new pg.Client();
-await client.connect();
-await client.query(fs.readFileSync('./bad.sql', 'utf8'));
-await client.end();
-*/
+{
+  const client = new pg.Client();
+  await client.connect();
+  await client.query(fs.readFileSync('./bad.sql', 'utf8')).then(() => {
+    console.log('node-postgress: No error!');
+  }).catch(e => {
+    console.log('node-postgers: yes error!', e.message);
+  });;
+  await client.end();
+}
+
